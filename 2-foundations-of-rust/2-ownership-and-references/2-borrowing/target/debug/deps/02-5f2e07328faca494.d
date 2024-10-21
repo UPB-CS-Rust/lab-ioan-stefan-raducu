0a51a1a322fc9182cbc0ab80560fdead25f6eba6
Neuer <istefanraducu@gmail.com>
2024-10-21 19:31:44 +0300

    lab2 - Raducu Ioan Stefan

---
+++ 2-foundations-of-rust/2-ownership-and-references/2-borrowing/target/debug/deps/02-5f2e07328faca494.d
@@ -0,0 +1,5 @@
+/home/neuer/Uni/Anul2/PCLP4-soft/lab-ioan-stefan-raducu/2-foundations-of-rust/2-ownership-and-references/2-borrowing/target/debug/deps/02-5f2e07328faca494: src/bin/02.rs
+
+/home/neuer/Uni/Anul2/PCLP4-soft/lab-ioan-stefan-raducu/2-foundations-of-rust/2-ownership-and-references/2-borrowing/target/debug/deps/02-5f2e07328faca494.d: src/bin/02.rs
+
+src/bin/02.rs:
